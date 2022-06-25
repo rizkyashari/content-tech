2780c18bf7be0f37b90c61b9d170d858e132303d
Muhammad Rizky Ashari <rizky.ashari@zeniuseducation.com>
2022-06-25 10:04:19 +0700

    updates

--- learn-functional-programming-with-clojure/01-basic-1/02-clojure-primitive-data-types.clj
+++ learn-functional-programming-with-clojure/01-basic-1/02-clojure-primitive-data-types.clj
@@ -28,5 +28,26 @@ false
 (str \i \n \d \o \n \e \s \i \a)
 (str \a \s \h \a \r \i)
 
+;; 4.1. Simple Types
+;; Clojure is built on top of the JVM, 
+;; and as such we have access to the same standard types as any other Java application. 
+;; Types are typically inferred automatically and don't need to be specified explicitly.
+;; For example:
+
+123 ; Long
+1.23 ; Double
+"Hello" ; String
+true ; Boolean
+
+;; We can specify some more complicated types as well, using special prefixes or suffixes:
+
+42N ; clojure.lang.BigInt
+3.14159M ; java.math.BigDecimal
+1/3 ; clojure.lang.Ratio
+#"[A-Za-z]+" ; java.util.regex.Pattern
+
+;; Note that the clojure.lang.BigInt type is used instead of java.math.BigInteger. 
+;; This is because the Clojure type has some minor optimizations and fixes.
+
 
 
