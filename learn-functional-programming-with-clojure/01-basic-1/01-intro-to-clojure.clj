c7f2e349936f9b45acd37fcf252c74f42d4613d1
rizky-ashari <rizy.ashari@zeniueseducation.com>
2022-06-25 15:33:26 +0700

    update intro to clojure

--- learn-functional-programming-with-clojure/01-basic-1/01-intro-to-clojure.clj
+++ learn-functional-programming-with-clojure/01-basic-1/01-intro-to-clojure.clj
@@ -15,12 +15,12 @@
 ;; baik dalam bentuk List yang lebih banyak atau nilai sederhana.
 ;; Sebagai contoh:
 
-(+ 1 2) ; = 3
+(+ 3 4) ; = 7
 
 ;; Ini adalah daftar yang terdiri dari tiga elemen. 
 ;; Simbol “+” menunjukkan bahwa kita sedang melakukan panggilan – penambahan. 
 ;; Elemen yang tersisa kemudian digunakan dengan panggilan ini. 
-;; Dengan demikian, ini mengevaluasi ke "1 + 2".
+;; Dengan demikian, ini mengevaluasi ke "3 + 4".
 ;; Dengan menggunakan sintaks List di sini, 
 ;; ini dapat diperpanjang secara simple. Misalnya, kita dapat melakukan:
 
@@ -29,8 +29,8 @@
 ;; Perhatikan juga karakter titik koma. 
 ;; Ini digunakan di Clojure untuk menunjukkan komentar dan bukan akhir dari ekspresi seperti yang kita lihat di Java.
 
-;; Data & Functions
-(* 2 3)
-(+ 3 4)
-(/ (- 58 2) 8)
-;; * / - dan + disebut sebagai function. Di clojure, function ditulis di depan.
+;; Rewrite following algebraic expression as a Clojure expression!
+
+; 2 * 3
+; (58 – 2) / 8
+; (7 + 3 * 4 + 5) / 10
